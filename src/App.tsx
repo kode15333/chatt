3cdb337559ae05e33ab99f01922bd18648ab91f9
Will <kode15333@gmail.com>
2021-03-29 07:52:35 +0900

    컴포넌트 구성

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,102 @@
-import React from 'react';
+import React, {Component, ReactNode, useEffect, useState} from "react";
+import {
+    Route,
+    BrowserRouter as Router,
+    Switch,
+    Redirect
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Chat from "./pages/Chat";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { auth } from "./services/firebase";
 
-
-function App() {
-  return (
-    <div className="App">
-        dksldjl
-
-    </div>
-  );
+interface PrivateProps {
+    path?: string;
+    component?: ReactNode;
+    authenticated?: boolean;
 }
+
+function PrivateRoute({ component: Component, authenticated, ...rest } : PrivateProps) {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                authenticated === true ? (
+                    // <Component {...props} />
+                    <Home/>
+                ) : (
+                    <Redirect
+                        to={{ pathname: "/login", state: { from: props.location } }}
+                    />
+                )
+            }
+        />
+    );
+}
+
+function PublicRoute({ component: Component, authenticated, ...rest } : PrivateProps) {
+    return (
+        <Route
+            {...rest}
+            render={props =>
+                authenticated === false ? (
+                    // <Component  />
+                    <Home/>
+                ) : (
+                    <Redirect to="/chat" />
+                )
+            }
+        />
+    );
+}
+
+const App = () => {
+    const [authenticated, setAuthenticated] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        auth().onAuthStateChanged(user => {
+            if (user) {
+                setAuthenticated(true);
+                setLoading(false);
+            } else {
+                setAuthenticated(false);
+                setLoading(false);
+            }
+        });
+    })
+
+    return loading ? (
+        <div className="spinner-border text-success" role="status">
+            <span className="sr-only">Loading...</span>
+        </div>
+    ) : (
+        <Router>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <PrivateRoute
+                    path="/chat"
+                    authenticated={authenticated}
+                    component={Chat}
+                />
+                <PublicRoute
+                    path="/signup"
+                    authenticated={authenticated}
+                    component={Signup}
+                />
+                <PublicRoute
+                    path="/login"
+                    authenticated={authenticated}
+                    component={Login}
+                />
+            </Switch>
+        </Router>
+    );
+}
+
+
+
+
 
 export default App;
