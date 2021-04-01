1df55341655de5dd3cf7d07751664aa94ba538a7
Will <kode15333@gmail.com>
2021-04-02 08:36:22 +0900

    컴포넌트 구성(Footer, Header)

--- src/App.tsx
+++ src/App.tsx
@@ -11,63 +11,35 @@ import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import { auth } from "./services/firebase";
 
-interface PrivateProps {
-    path?: string;
-    component?: ReactNode;
-    authenticated?: boolean;
+interface loginType {
+    authenticated: boolean;
+    loading: boolean
 }
-
-function PrivateRoute({ component: Component, authenticated, ...rest } : PrivateProps) {
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                authenticated === true ? (
-                    // <Component {...props} />
-                    <Home/>
-                ) : (
-                    <Redirect
-                        to={{ pathname: "/login", state: { from: props.location } }}
-                    />
-                )
-            }
-        />
-    );
-}
-
-function PublicRoute({ component: Component, authenticated, ...rest } : PrivateProps) {
-    return (
-        <Route
-            {...rest}
-            render={props =>
-                authenticated === false ? (
-                    // <Component  />
-                    <Home/>
-                ) : (
-                    <Redirect to="/chat" />
-                )
-            }
-        />
-    );
-}
-
 const App = () => {
-    const [authenticated, setAuthenticated] = useState(false);
-    const [loading, setLoading] = useState(true);
+    const [login, setLogin] = useState<loginType>({
+        authenticated : false,
+        loading: true
+    });
 
     useEffect(() => {
         auth().onAuthStateChanged(user => {
             if (user) {
-                setAuthenticated(true);
-                setLoading(false);
+                setLogin({
+                    ...login,
+                    authenticated : true,
+                    loading: false
+                })
             } else {
-                setAuthenticated(false);
-                setLoading(false);
+                setLogin({
+                    ...login,
+                    authenticated : false,
+                    loading: false
+                })
             }
         });
-    })
+   })
 
-    return loading ? (
+    return login.loading ? (
         <div className="spinner-border text-success" role="status">
             <span className="sr-only">Loading...</span>
         </div>
@@ -75,21 +47,6 @@ const App = () => {
         <Router>
             <Switch>
                 <Route exact path="/" component={Home} />
-                <PrivateRoute
-                    path="/chat"
-                    authenticated={authenticated}
-                    component={Chat}
-                />
-                <PublicRoute
-                    path="/signup"
-                    authenticated={authenticated}
-                    component={Signup}
-                />
-                <PublicRoute
-                    path="/login"
-                    authenticated={authenticated}
-                    component={Login}
-                />
             </Switch>
         </Router>
     );
