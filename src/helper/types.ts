5e3b8a30e83176ae820d6f1df496f764f3b4fb5a
Will <kode15333@gmail.com>
2021-04-08 08:02:19 +0900

    Chats component 구현중

--- src/helper/types.ts
+++ src/helper/types.ts
@@ -11,9 +11,19 @@ export interface MessageInfo {
     uid: string;
 }
 
-export type SnapShopType =  (string | number | boolean)[]
+export type SnapShopType = (string | number | boolean)[]
 
 export type Info = {
     error: string | null
     email: string;
-    password: string; };
+    password: string;
+};
+
+export interface ChatInfo {
+    user: firebase.User | null;
+    chats: SnapShopType;
+    content: string;
+    readError: string | null;
+    writeError: string | null;
+    loadingChats: boolean;
+}
--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Header from "../components/Header";
+import {ChatInfo} from "../helper/types";
+import {auth} from "../services/firebase";
 
 const Chat = () => {
+    const [chatInfo, setChatInfo] = useState<ChatInfo>({
+        user: auth().currentUser,
+        chats: [],
+        content: '',
+        readError: null,
+        writeError: null,
+        loadingChats: false
+    })
     return (
         <div>
-
         </div>
     );
 };
