1e204601d7e16337677e4d5d27a9466987dd26ae
Will <kode15333@gmail.com>
2021-04-09 06:44:43 +0900

    useRef 구현

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -1,7 +1,7 @@
-import React, {useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import Header from "../components/Header";
-import {ChatInfo} from "../helper/types";
-import {auth} from "../services/firebase";
+import {ChatInfo, SnapShopType} from "../helper/types";
+import {auth, db} from "../services/firebase";
 
 const Chat = () => {
     const [chatInfo, setChatInfo] = useState<ChatInfo>({
@@ -12,8 +12,60 @@ const Chat = () => {
         writeError: null,
         loadingChats: false
     })
+
+    const myRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        setChatInfo({
+            ...chatInfo,
+            readError: null,
+            loadingChats: true
+        })
+        const chatArea = myRef.current;
+        try {
+            db.ref("chats").on("value", snapshot => {
+                let chats :  any = [];
+                snapshot.forEach((snap) => {
+                    chats.push(snap.val());
+                });
+                chats.sort(function (a, b) { return a.timestamp - b.timestamp })
+                chatArea && chatArea.scrollBy(0, chatArea.scrollHeight);
+                setChatInfo({
+                    ...chatInfo,
+                    chats,
+                    readError: null,
+                    loadingChats: false
+                })
+            });
+        } catch (error) {
+            setChatInfo({
+                ...chatInfo,
+                readError: error.message,
+                loadingChats: false
+            })
+        }
+        return () => {
+
+        };
+    }, []);
+
+
+    const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const {name, value} = e.target;
+        setChatInfo({
+            ...chatInfo,
+            [name]: value
+        })
+    }
     return (
         <div>
+            <Header/>
+
+            <div className="chat-area" ref={myRef}>
+                {/* loading indicator */}
+                {chatInfo.loadingChats ? <div className="spinner-border text-success" role="status">
+                    <span className="sr-only">Loading...</span>
+                </div> : ""}
         </div>
     );
 };
